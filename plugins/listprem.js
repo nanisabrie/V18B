996f720d45a212b8738d3c73cd202a38bc52fdde
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-04-18 03:03:43 +0800

    Update listprem.js
    Pp

--- plugins/listprem.js
+++ plugins/listprem.js
@@ -11,6 +11,5 @@ let handler = async (m, { conn }) => {
 handler.help = ['premlist']
 handler.tags = ['owner']
 handler.command = /^(listprem|premlist)$/i
-handler.owner = true
 
 module.exports = handler
